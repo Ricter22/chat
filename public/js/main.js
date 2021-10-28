0a365913d4f1abf8e434cc8316ab2b369afff8dc
juaordmar <juaordmar@alum.us.es>
2021-10-28 15:55:30 +0200

    Server added

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+const socket = io();
+
+socket.on('message', message => {
+    console.log(message); //the messages that we emit from the server are catched here
+}); 
---
+++ server.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', socket => { //socket is a parameter    
+    //Welcome current user
+    socket.emit('message', 'Welcome to a chat');
+    
+    //Broadcast when a user connects    
+    socket.broadcast.emit('message', 'A user has joined the chat'); //it notifies me when 
+                                                                    //someone different than me connects
+    //io.emit is for all clients in general
+    
+    //Runs when client disconnect
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+});
+
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
